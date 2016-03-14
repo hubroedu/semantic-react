6edac28afa985aef8149676f2eb0f6330cf3fd0a
jorgeer <jfemail3@gmail.com>
2016-03-14 18:03:57 +0100

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "hubroedu:semantic-react",
-  version: "0.1.4",
+  version: "0.1.5",
   summary: "Semantic-ui as useful react components, based on react-semantify, with additional functionality.",
   git: "https://github.com/hubroedu/semantic-react",
   documentation: "README.md"
